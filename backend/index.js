ff0b6cf2209d45994225b87104127412caf9d430
Anurag Bose <anuragbasu039@gmail.com>
2024-08-31 09:06:52 +0530

    first commit

--- backend/index.js
+++ backend/index.js
@@ -10,23 +10,23 @@ import cors from "cors";
 databaseConnection();
 
 dotenv.config({
-    path:".env"
+    path: ".env"
 })
 
 const app = express();
 //middlewares 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:3000',
-    credentials:true
+    origin: 'http://localhost:3000',
+    credentials: true
 }
 app.use(cors(corsOptions));
- 
+
 // api
 app.use("/api/v1/user", userRoute);
 
-app.listen(process.env.PORT,() => {
+app.listen(process.env.PORT, () => {
     console.log(`Server listen at port ${process.env.PORT}`);
 });
